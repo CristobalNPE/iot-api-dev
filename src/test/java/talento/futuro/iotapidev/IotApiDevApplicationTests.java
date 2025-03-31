fb275836081c7d79a0b25d5ff92caf382691542c
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-03-31 15:07:44 -0300

    scope test to profile "test" to avoid loading datasource bean from general config

--- src/test/java/talento/futuro/iotapidev/IotApiDevApplicationTests.java
+++ src/test/java/talento/futuro/iotapidev/IotApiDevApplicationTests.java
@@ -3,9 +3,12 @@ package talento.futuro.iotapidev;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import talento.futuro.iotapidev.config.DataSourceConfig;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class IotApiDevApplicationTests {
 
     @Test
