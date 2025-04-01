dd2ed81daee3a72b58cf943158e08132cd0c1f7e
krlozzheba <carloss.ramirezlopez@gmail.com>
2025-04-01 19:19:03 -0300

    Se crea nuevo Servicio Location para Admin

---
+++ src/main/java/talento/futuro/iotapidev/service/AdminLocationService.java
@@ -0,0 +1,88 @@
+package talento.futuro.iotapidev.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import talento.futuro.iotapidev.dto.LocationAdminRequest;
+import talento.futuro.iotapidev.dto.LocationResponse;
+import talento.futuro.iotapidev.exception.CompanyNotFoundException;
+import talento.futuro.iotapidev.exception.LocationNotFoundException;
+import talento.futuro.iotapidev.mapper.LocationMapper;
+import talento.futuro.iotapidev.model.Company;
+import talento.futuro.iotapidev.model.Location;
+import talento.futuro.iotapidev.repository.CompanyRepository;
+import talento.futuro.iotapidev.repository.LocationRepository;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AdminLocationService {
+
+    private final LocationRepository locationRepository;
+    private final LocationMapper locationMapper;
+    private final CompanyRepository companyRepository;
+    private final AuthService authService;
+
+     public Page<LocationResponse> adminFindAllLocations(Pageable pageable) {
+        Page<Location> locations = locationRepository.findAll(pageable);
+
+        return locations.map(locationMapper::toLocationResponse);
+    }
+
+    public LocationResponse adminCreateLocation(@Valid LocationAdminRequest request) {
+        if (request.companyId() == null) {
+            throw new CompanyNotFoundException(null);
+        }
+        Company company = companyRepository.findById(request.companyId())
+                                           .orElseThrow(() -> new CompanyNotFoundException(request.companyId()));
+
+        Location newLocation = Location
+                .builder()
+                .company(company)
+                .name(request.name())
+                .city(request.city())
+                .country(request.country())
+                .meta(request.meta())
+                .build();
+
+        Location saved = locationRepository.save(newLocation);
+
+        return locationMapper.toLocationResponse(saved);
+    }
+
+    public LocationResponse adminFindLocationById(Integer locationId) {
+        return locationRepository.findById(locationId)
+                                 .map(locationMapper::toLocationResponse)
+                                 .orElseThrow(() -> new LocationNotFoundException(locationId));
+    }
+
+    public LocationResponse adminUpdateLocation(Integer locationId, @Valid LocationAdminRequest request) {
+        if (request.companyId() == null) {
+            throw new CompanyNotFoundException(null);
+        }
+        Company company = companyRepository.findById(request.companyId())
+                                           .orElseThrow(() -> new CompanyNotFoundException(request.companyId()));
+
+        Location location = locationRepository.findById(locationId)
+                                              .orElseThrow(() -> new LocationNotFoundException(locationId));
+
+        location.setCompany(company);
+        location.setName(request.name());
+        location.setCity(request.city());
+        location.setCountry(request.country());
+        location.setMeta(request.meta());
+
+        return locationMapper.toLocationResponse(locationRepository.save(location));
+    }
+
+    public void adminDeleteLocation(Integer locationId) {
+        if (!locationRepository.existsById(locationId)) {
+            throw new LocationNotFoundException(locationId);
+        }
+        locationRepository.deleteById(locationId);
+    }
+}
