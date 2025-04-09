366c92daab79072cda7fede81fcf9a442b2ec1c8
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-08 21:58:24 -0400

    obscure sensor api key for responses

--- src/main/java/talento/futuro/iotapidev/exception/InvalidSensorApiKeyException.java
+++ src/main/java/talento/futuro/iotapidev/exception/InvalidSensorApiKeyException.java
@@ -3,6 +3,12 @@ package talento.futuro.iotapidev.exception;
 
 public class InvalidSensorApiKeyException extends RuntimeException {
     public InvalidSensorApiKeyException(String apikey) {
-        super("Invalid API KEY for sensor: %s.".formatted(apikey));
+        super("Invalid API KEY for sensor API KEY: %s.".formatted(obscureApiKey(apikey)));
     }
+
+    private static String obscureApiKey(String apiKey) {
+        return apiKey.substring(0, 3) + "..." + apiKey.substring(apiKey.length() - 3);
+    }
+
 }
+
