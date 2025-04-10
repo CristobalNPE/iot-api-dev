60b5208c9321d923fb4f66f62aac2009037f47b3
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-09 20:55:17 -0400

    disable admin intializer during testing to avoid affecting build process when no env is configured

--- src/main/java/talento/futuro/iotapidev/util/AdminUserInitializer.java
+++ src/main/java/talento/futuro/iotapidev/util/AdminUserInitializer.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -12,6 +13,7 @@ import talento.futuro.iotapidev.repository.AdminRepository;
 
 @Slf4j
 @Component
+@Profile("!test")
 @RequiredArgsConstructor
 public class AdminUserInitializer {
 
