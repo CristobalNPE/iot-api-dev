89571355c42dae68c02f903b44850c2578df8349
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-04-11 20:28:58 -0400

    deploy

--- src/main/java/talento/futuro/iotapidev/config/DataSourceConfig.java
+++ src/main/java/talento/futuro/iotapidev/config/DataSourceConfig.java
@@ -3,26 +3,42 @@ package talento.futuro.iotapidev.config;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
+import java.net.URI;
 
 @Configuration
 @RequiredArgsConstructor
-@PropertySource("classpath:database.properties")
 @Profile("!test")
 public class DataSourceConfig {
 
-    private final Environment environment;
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
 
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
-        config.setDriverClassName(environment.getRequiredProperty("driver"));
-        config.setJdbcUrl(environment.getRequiredProperty("url"));
-        config.setUsername(environment.getRequiredProperty("dbUser"));
-        config.setPassword(environment.getRequiredProperty("dbPassword"));
+        config.setJdbcUrl(convertPostgresUrlToJdbc(datasourceUrl));
         return new HikariDataSource(config);
+    }
+
+    public static String convertPostgresUrlToJdbc(String postgresUrl) {
+        URI uri = URI.create(postgresUrl);
+        String host = uri.getHost();
+        int port = uri.getPort();
+        String path = uri.getPath();
+        String[] userInfo = uri.getUserInfo().split(":");
+
+        return String.format("jdbc:postgresql://%s:%d%s?user=%s&password=%s&sslmode=disable",
+                host,
+                port,
+                path,
+                userInfo[0],
+                userInfo[1]
+        );
     }
 }
